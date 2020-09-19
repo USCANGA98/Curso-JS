01231a3e44f2d314d2fa8f191c2ca316e3d062ee
USCANGA <macarioluisangel006@gmail.com>
2020-09-19 12:43:20 -0500

    Se agregó un NavBar, un bg-dark y datos completos de los usuarios

--- js04.js
+++ js04.js
@@ -6,10 +6,24 @@ function traer() {
         .then(data => {
             console.log(data.results)
             contenido.innerHTML = `
-            <img src="${data.results[0].picture.large}" width="100px" class="img-fluid rounded-circle">
-            <p>Nombre: ${data.results[0].name.title} ${data.results[0].name.first} ${data.results[0].name.last} </p>
+            <img src="${data.results[0].picture.large}" width="150px" class="img-fluid rounded-circle">
+            <h5 class="text-white mt-3" align="justify">
+            Nombre: ${data.results[0].name.title} ${data.results[0].name.first} ${data.results[0].name.last} <br>
+            Edad: ${data.results[0].registered.age} <br>
+            Fecha: ${data.results[0].registered.date} <br>
+            Telefono: ${data.results[0].cell}<br>
+            Email: ${data.results[0].email}<br>
+            Ciudad: ${data.results[0].location.city}<br>
+            País: ${data.results[0].location.country}<br>
+            Estado: ${data.results[0].location.state}<br>
+            Direccion: ${data.results[0].location.street.name} ${data.results[0].location.street.number} 
+
+            
+            
+            
+            </h5>
             `
         })
 
-        
+
 }
