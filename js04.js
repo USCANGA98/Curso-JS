2688075bdb2daa3715a0fd549aa349860618e74f
USCANGA <macarioluisangel006@gmail.com>
2020-09-21 18:19:46 -0500

    Se añadió la clase card y ajustó la info

--- js04.js
+++ js04.js
@@ -5,9 +5,9 @@ function traer() {
         .then(res => res.json())
         .then(data => {
             console.log(data.results)
-            contenido.innerHTML = `
-            <img src="${data.results[0].picture.large}" width="150px" class="img-fluid rounded-circle">
-            <h5 class="text-white mt-3" align="justify">
+            contenido.innerHTML = `<div class="card-body" style="width: 60rem; ">
+            <center><img src="${data.results[0].picture.large}" width="190px" class="img-fluid rounded-circle "></center>
+            <h5 class="text-dark mt-3 card-text" align="justify" >
             Nombre: ${data.results[0].name.title} ${data.results[0].name.first} ${data.results[0].name.last} <br>
             Edad: ${data.results[0].registered.age} <br>
             Fecha: ${data.results[0].registered.date} <br>
@@ -17,11 +17,9 @@ function traer() {
             País: ${data.results[0].location.country}<br>
             Estado: ${data.results[0].location.state}<br>
             Direccion: ${data.results[0].location.street.name} ${data.results[0].location.street.number} 
-
-            
-            
-            
             </h5>
+            </div>
+            
             `
         })
 
